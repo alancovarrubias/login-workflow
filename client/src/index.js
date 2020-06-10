08ba345bca37b01e3b39bc06fabaeb68555cdd5b
Alan Covarrubias <aecovarru@gmail.com>
2020-06-09 21:35:06 -0700

    setup client

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+
+function FullApp() {
+  return <App />;
+}
+
+ReactDOM.render(<FullApp />, document.getElementById("app"));
--- login/spec/models/user_spec.rb
+++ login/spec/models/user_spec.rb
@@ -9,6 +9,5 @@ RSpec.describe User, type: :model do
     it { should have_secure_password }
     it { should validate_presence_of(:username) }
     it { should validate_uniqueness_of(:username) }
-    it { should validate_presence_of(:password) }
   end
 end
